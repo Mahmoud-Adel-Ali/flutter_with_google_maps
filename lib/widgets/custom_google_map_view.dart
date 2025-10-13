a5c665bcc5020d3b8fc9b1df8daf90c0c6d8f5a4
Mahmoud Adel Ali <mahmoudadela413@gmail.com>
2025-10-12 21:23:20 -0700

    Polygons

--- lib/widgets/custom_google_map_view.dart
+++ lib/widgets/custom_google_map_view.dart
@@ -18,15 +18,19 @@ class _CustomGoogleMapViewState extends State<CustomGoogleMapView> {
   Set<Marker> markers = {};
 
   Set<Polyline> polylines = {};
+  Set<Polygon> polygons = {};
 
   @override
   void initState() {
     super.initState();
     initMarkers();
-    initialCameraPosition =
-        const CameraPosition(target: LatLng(100, 100), zoom: 12);
+    initialCameraPosition = const CameraPosition(
+      target: LatLng(100, 100),
+      zoom: 12,
+    );
 
     initPolylines();
+    initPolygons();
   }
 
   @override
@@ -41,6 +45,7 @@ class _CustomGoogleMapViewState extends State<CustomGoogleMapView> {
       body: Stack(
         children: [
           GoogleMap(
+            polygons: polygons,
             polylines: polylines,
             markers: markers,
             initialCameraPosition: initialCameraPosition,
@@ -67,37 +72,43 @@ class _CustomGoogleMapViewState extends State<CustomGoogleMapView> {
                 mapController.animateCamera(
                   // CameraUpdate.newCameraPosition(cameraPosition)
                   CameraUpdate.newLatLng(
-                      const LatLng(29.8552649548856, 29.8552649548856)),
+                    const LatLng(29.8552649548856, 29.8552649548856),
+                  ),
                 );
               },
               child: const Text('Zoom In'),
             ),
-          )
+          ),
         ],
       ),
     );
   }
 
   void initMapStyle() async {
-    var nightMapStyle = await DefaultAssetBundle.of(context)
-        .loadString('assets/map_styles/night_map_style.json');
+    var nightMapStyle = await DefaultAssetBundle.of(
+      context,
+    ).loadString('assets/map_styles/night_map_style.json');
 
     mapController.setMapStyle(nightMapStyle);
   }
 
   Future<Uint8List> getImageFromRowData(String image, double width) async {
     ByteData data = await rootBundle.load(image);
-    ui.Codec codec = await ui.instantiateImageCodec(data.buffer.asUint8List(),
-        targetWidth: width.toInt());
+    ui.Codec codec = await ui.instantiateImageCodec(
+      data.buffer.asUint8List(),
+      targetWidth: width.toInt(),
+    );
     ui.FrameInfo imageFram = await codec.getNextFrame();
-    var imageByteData =
-        await imageFram.image.toByteData(format: ui.ImageByteFormat.png);
+    var imageByteData = await imageFram.image.toByteData(
+      format: ui.ImageByteFormat.png,
+    );
     return imageByteData!.buffer.asUint8List();
   }
 
   void initMarkers() async {
     var markerIcon = BitmapDescriptor.bytes(
-        await getImageFromRowData('assets/images/marker_icon.png', 150));
+      await getImageFromRowData('assets/images/marker_icon.png', 150),
+    );
     places.map((place) {
       markers.add(
         Marker(
@@ -126,6 +137,21 @@ class _CustomGoogleMapViewState extends State<CustomGoogleMapView> {
       patterns: [PatternItem.dot],
     );
     polylines.add(polyline);
+  }
+
+  void initPolygons() {
+    const polygon = Polygon(
+      polygonId: PolygonId('1'),
+      points: [
+        LatLng(26.55800060103205, 31.697322615598104),
+        LatLng(26.559316417027457, 31.69567483739066),
+        LatLng(26.558651536641364, 31.695643649160477),
+      ],
+      strokeWidth: 5,
+      strokeColor: Colors.greenAccent,
+      fillColor: Colors.greenAccent,
+    );
+    polygons.add(polygon);
   }
 }
 
