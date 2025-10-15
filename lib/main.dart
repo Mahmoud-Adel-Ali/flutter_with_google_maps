c2e3b797347f0fb76b5d59f10a5199144b25c47c
Mahmoud Adel Ali <mahmoudadela413@gmail.com>
2025-10-15 14:11:33 -0700

    The Starting of create RouteTrackerApp.

--- lib/main.dart
+++ lib/main.dart
@@ -2,29 +2,43 @@ import 'package:device_preview/device_preview.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-import 'widgets/custom_google_map_view.dart';
+import 'google_map/google_map_view.dart';
 
 void main() {
   runApp(
     DevicePreview(
       enabled: !kReleaseMode,
-      builder: (context) => const TestGoogleMapwithFlutter(),
+      // builder: (context) => const TestGoogleMapwithFlutter(),
+      builder: (context) => const RouteTrackerApp(),
     ),
   );
 }
 
-class TestGoogleMapwithFlutter extends StatelessWidget {
-  const TestGoogleMapwithFlutter({super.key});
+
+// class TestGoogleMapwithFlutter extends StatelessWidget {
+//   const TestGoogleMapwithFlutter({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       theme: ThemeData(
+//         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+//         useMaterial3: true,
+//       ),
+//       home: const CustomGoogleMapView(),
+//     );
+//   }
+// }
+
+class RouteTrackerApp extends StatelessWidget {
+  const RouteTrackerApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const CustomGoogleMapView(),
+      home: const GoogleMapView(),
     );
   }
 }
